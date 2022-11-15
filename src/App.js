067200d3fc70a04ca9e4343e38fec11529c2b475
Aghmaz <obs24by7@.com>
2022-11-15 12:59:29 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import todo from "./image/todo.png";
+import obs from "./image/obs.png";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// to get the data 
+
+const getLocalItmes = () => {
+    let list = localStorage.getItem('lists');
+    console.log(list);
+
+    if (list) {
+        return JSON.parse(localStorage.getItem('lists'));
+    } else {
+        return [];
+    }
 }
 
-export default App;
+// declaring a arrow function 
+const Todo = () => {
+// using states 
+    const [inputData, setInputData] = useState('');
+    const [items, setItems] = useState(getLocalItmes());
+    const [toggleSubmit, setToggleSubmit] = useState(true);
+    const [isEditItem, setIsEditItem] = useState(null);
+
+    const addItem = () => {
+      // applying check 
+      if (!inputData) {
+            alert('You did not write any data');
+        } else if(inputData && !toggleSubmit) {
+            setItems(
+                items.map((elem) => {
+                    if (elem.id === isEditItem) {
+                        return { ...elem, name: inputData }
+                    }
+                    return elem;
+                })
+            )
+                 setToggleSubmit(true);
+
+                 setInputData('');
+
+                 setIsEditItem(null);
+        } else {
+            const allInputData = { id: new Date().getTime().toString(), name:inputData }
+            setItems([...items, allInputData]);
+            setInputData('')
+        }
+    }
+
+    
+    // delete the items
+    const deleteItem = (index) => {
+        const updateditems = items.filter((elem) => {
+            return index !== elem.id;
+        });
+
+        setItems(updateditems);
+    }
+
+// edit the item
+//     When user clikc on edit button 
+
+// 1: get the id and name of the data which user clicked to edit
+// 2: set the toggle mode to change the submit button into edit button
+// 3: Now update the value of the setInput with the new updated value to edit. 
+// 4: To pass the current element Id to new state variable for reference 
+    
+    
+    const editItem = (id) => {
+        let newEditItem = items.find((elem) => {
+            return elem.id === id
+        });
+        console.log(newEditItem);
+
+        setToggleSubmit(false);
+
+        setInputData(newEditItem.name);
+
+        setIsEditItem(id);
+
+    }
+    
+
+    // remove all 
+    const removeAll = () => {
+         setItems([]);
+    }
+
+    // add data to localStorage
+    useEffect(() => {
+       localStorage.setItem('lists', JSON.stringify(items))
+    }, [items]);
+
+    return (
+        <>
+            <div className="main-div">
+              <div className='row'>
+                <div style={{marginTop:"-8rem"}} className='col'>
+
+              <div className='obs'>   <img src={obs} alt="Online-Business-Solutions" width={"350px"}/>
+              <h3 style={{fontSize:"2rem",color:"white",marginTop:"-8rem"}}> Todo-list | Add-List | Remove-List | Update-List </h3>
+              </div>
+                  
+                </div>
+                <div style={{marginTop:"4rem"}} className='col'>
+                  
+                <div className="child-div">
+                    <figure>
+                        <img src={todo} alt="todologo" />
+                        <figcaption>Add Your List Here ✌</figcaption>
+                    </figure>
+
+                    <div className="addItems">
+                        <input type="text" placeholder="✍ Add Items..."
+                           value={inputData} 
+                           onChange={(e) => setInputData(e.target.value) }
+                        />
+                        {
+                            toggleSubmit ? <i className="fa fa-plus add-btn" title="Add Item" onClick={addItem}></i> :
+                                 <i className="far fa-edit add-btn" title="Update Item" onClick={addItem}></i>
+                        }
+                       
+                    </div>
+
+                    <div className="showItems">
+                        
+                        {
+                            items.map((elem) => {
+                                return (
+                                    <div className="eachItem" key={elem.id}>
+                                        <h3>{elem.name}</h3>
+                                        <div className="todo-btn">
+                                            <i className="far fa-edit add-btn" title="Edit Item" onClick={() => editItem(elem.id)}></i>
+                                            <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={() => deleteItem(elem.id)}></i>
+                                        </div>
+                                  </div>
+                                )
+                            })
+
+                        }
+                       
+                    </div>
+                
+                    {/* clear all button  */}
+                    <div className="showItems">
+                        <button className="btn effect04" data-sm-link-text="Remove All" onClick={removeAll}><span> CHECK LIST </span> </button>
+                    </div>
+                </div>
+                </div>
+              </div>
+          
+          </div>  
+        </>
+    )
+}
+
+export default Todo
